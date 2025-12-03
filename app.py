2c97ef7fb0b901e4f604a697da9b7ec31dce8b91
Bocar-DIACK <diack.bocar-mamoudou@ugb.edu.sn>
2025-12-03 00:51:16 +0000

    Fix app variable for Gunicorn deployment

--- app.py
+++ app.py
@@ -15,6 +15,8 @@ def create_app():
     
     return app
 
+# Instance Flask globale pour Gunicorn (Render)
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
     app.run(debug=True, host='0.0.0.0', port=5000)
